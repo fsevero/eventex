68bcbf2913b65f0c219be2722c8cae0bae880853
Severo <severo.fabricio@gmail.com>
2016-01-26 17:05:02 +0000

    Managers

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.shortcuts import resolve_url as r
 
+from eventex.core.managers import KindQuerySet, PeriodManager
+
+
 class Speaker(models.Model):
     name = models.CharField(max_length=255, verbose_name='nome')
     slug = models.SlugField(verbose_name='slug')
@@ -32,6 +35,8 @@ class Contact(models.Model):
     kind = models.CharField('tipo', max_length=1, choices=KINDS)
     value = models.CharField('valor', max_length=255)
 
+    objects = KindQuerySet.as_manager()
+
     class Meta:
         verbose_name='contato'
         verbose_name_plural='contatos'
@@ -46,6 +51,8 @@ class Talk(models.Model):
     description = models.TextField('descrição', blank=True)
     speakers = models.ManyToManyField('Speaker', verbose_name='palestrantes',
                                       blank=True)
+
+    objects = PeriodManager()
 
     class Meta:
         verbose_name_plural='palestras'
